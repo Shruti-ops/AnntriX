88440288eccf7b26f5658f17fe1535a6de65fbad
Shruti  Meshram <shruti.techfirm@gmail.com>
2024-02-12 17:18:53 +0530

    all the initial files added
    added all the initial files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [  "./src/**/*.{js,jsx,ts,tsx}", ],
+  theme: {
+    extend: {
+      colors: {
+        '#1D1D36': '#1D1D36',
+        '#A4A3DC': '#A4A3DC',
+      },
+    },
+  },
+  plugins: [],
+}
+
